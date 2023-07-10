e96fcd2c67545a90ffef56e496a2f3a85f1a19c2
johnchristismyname <139159120+johnchristismyname@users.noreply.github.com>
2023-07-11 03:24:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector("#blackTea").addEventListener("click", () => {
+    document.querySelector(".front").style.display = "none";
+    document.querySelector(".blackTeaPage").style.display = "block";
+});
+
+document.querySelector("#greenTea").addEventListener("click", () => {
+    document.querySelector(".front").style.display = "none";
+    document.querySelector(".greenTeaPage").style.display = "block";
+});
+
+document.querySelector("#oolongTea").addEventListener("click", () => {
+    document.querySelector(".front").style.display = "none";
+    document.querySelector(".oolongTeaPage").style.display = "block";
+});
+
+document.querySelector("#milkyBubbleTea").addEventListener("click", () => {
+    document.querySelector(".front").style.display = "none";
+    document.querySelector(".milkyBubbleTeaPage").style.display = "block";
+});
